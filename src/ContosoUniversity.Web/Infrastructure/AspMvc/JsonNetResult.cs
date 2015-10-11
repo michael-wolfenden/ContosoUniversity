e1442e305d1f3867fab9f37acb11f5a062537bfc
Michael Wolfenden <michael.wolfenden@gmail.com>
2015-10-12 09:24:02 +1000

    Use JSON.Net for json serialization

---
+++ src/ContosoUniversity.Web/Infrastructure/AspMvc/JsonNetResult.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ContosoUniversity.Web.Infrastructure.AspMvc
+{
+    public class JsonNetResult : JsonResult
+    {
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        public JsonNetResult()
+        {
+            _serializerSettings = new JsonSerializerSettings()
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            response.ContentType = ContentType;
+            if (ContentType.IsNullOrWhiteSpace())
+            {
+                response.ContentType = "application/json";
+            }
+
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+
+            if (Data == null) return;
+
+            var formatting = Formatting.None;
+            if (HttpContext.Current != null && HttpContext.Current.IsDebuggingEnabled)
+            {
+                formatting = Formatting.Indented;
+            }
+
+            JsonTextWriter writer = new JsonTextWriter(response.Output)
+            {
+                Formatting = formatting
+            };
+
+            JsonSerializer
+                .Create(_serializerSettings)
+                .Serialize(writer, Data);
+
+            writer.Flush();
+        }
+    }
+}
